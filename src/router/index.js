e0598b020dd92f2041ed6dfe0a28e34a9ecd622e
jhyounyaho <jhyoun-yaho@hanmail.net>
2021-01-20 08:01:20 +0900

    라우터 객체를 별도 파일로 분리 (router/index.js)

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from '../App.vue'
+
+Vue.use(VueRouter)
+
+const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Page not found</div>' }
+
+const router = new VueRouter({
+  routes: [
+    {path: '/', component: App},
+    {path: '/login', component: Login},
+    {path: '*', component: NotFound}
+  ]
+})
+
+export default router;
